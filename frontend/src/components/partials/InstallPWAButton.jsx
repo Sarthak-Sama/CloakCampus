6b7b83374556a198dae768cb1d83eb0f7710db16
Sarthak-Sama <sarthak.saklani04@gmail.com>
2025-02-13 00:13:47 +0530

    fixed minor bug

--- frontend/src/components/partials/InstallPWAButton.jsx
+++ frontend/src/components/partials/InstallPWAButton.jsx
@@ -1,9 +1,10 @@
 import { RiMobileDownloadLine } from "@remixicon/react";
 import React, { useState, useEffect } from "react";
 
-const InstallPWAButton = (deferredPrompt, isInstalled, setIsInstalled) => {
+const InstallPWAButton = ({ deferredPrompt, isInstalled, setIsInstalled }) => {
   const handleInstallClick = () => {
     console.log("clicked");
+    console.log(deferredPrompt);
     if (deferredPrompt) {
       deferredPrompt.prompt();
       deferredPrompt.userChoice.then((choiceResult) => {
@@ -29,19 +30,6 @@ const InstallPWAButton = (deferredPrompt, isInstalled, setIsInstalled) => {
       </button>
     </>
   ) : null;
-
-  return (
-    <>
-      <hr className="border-zinc-600 rounded-full w-[80] mb-6" />
-      <button
-        className="flex items-center gap-2 opacity-70 hover:opacity-100 mb-5"
-        onClick={handleInstallClick}
-      >
-        <RiMobileDownloadLine />
-        Add App to your HomeScreen.
-      </button>
-    </>
-  );
 };
 
 export default InstallPWAButton;
